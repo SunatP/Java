1ca0d6b726e7a5f682a15c43d13b65d5c7f72eb8
Sunat.P <31818562+SunatP@users.noreply.github.com>
2018-06-19 21:40:00 +0700

    Create LetterPrinter.kt

---
+++ Week 1/LetterPrinter.kt
@@ -0,0 +1,20 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package LetterPrinter
+import java.time.Clock.system
+/**
+*
+* @author Acer-
+*/
+object LetterPrinter {
+  @JvmStatic fun main(args:Array<String>) {
+    println("XXXX")
+    println("X")
+    println("XXXX")
+    println(" X")
+    println("XXXX")
+  }
+}
